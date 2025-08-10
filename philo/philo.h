3a03542182973b50b3ef8824442ab8dad73f114b
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-10 20:23:46 +0200

    base structs created

--- philo/philo.h
+++ philo/philo.h
@@ -6,11 +6,46 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 19:52:13 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/10 19:52:26 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/10 20:23:27 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_philo t_philo;
+
+typedef struct s_data
+{
+	int				philo_amount;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				meal_amount;
+	int				end;
+	long			start_time;
+	t_philo			*philos;
+	pthread_t		monitor_thread;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	data;
+}					t_data;
+
+typedef struct s_philo
+{
+	int				id;
+	int				meals_eaten;
+	long			time_since_meal;
+	pthread_t		thread;
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t	right_fork;
+}					t_philo;
 
 #endif
