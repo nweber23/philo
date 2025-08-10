64201f54b98c9a82431a31b9d2695d3ff84e1804
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-10 21:02:52 +0200

    parsing done

--- philo/main.c
+++ philo/main.c
@@ -6,22 +6,61 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 20:27:12 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/10 20:31:26 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/10 20:58:20 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	check_positives(char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	num = ft_atoi(str);
+	if (num < 0)
+		return (0);
+	return (1);
+}
+
+static int	parse_arg(int argc, char **argv)
+{
+	if (argc != 5 && argc != 6)
+	{
+		printf("Usage: ./philo Numbers_of_Philos time_to_die ");
+		printf("time_to_eat time_to_sleep [numbers_of_meals]\n");
+		return (0);
+	}
+	if (!check_positives(argv[1]) || !check_positives(argv[2])
+		|| !check_positives(argv[3]) || !check_positives[4])
+		return (0);
+	if (argc == 6 && !check_positives(argv[5]))
+		return (0);
+	return (1);
+
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (parse_arg(argc, argv, &data) != 0)
+	if (!parse_arg(argc, argv, &data))
 		return (printf("Error\n"), 1);
-	if (init_data(&data) != 0)
-		return (printf("Init Error\n"), 1);
-	if (start_simulation(&data) != 0)
+	memset(&data, 0, sizeof(t_data));
+	if (!init_data(&data))
+		return (printf("Init Error\n"), cleanup(&data), 1);
+	if (!create_threads(&data))
 		return (printf("Start Error\n"), cleanup(&data), 1);
+	join_threads(&data);
 	cleanup(&data);
 	return (0);
 }
--- philo/utils.c
+++ philo/utils.c
@@ -6,8 +6,33 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 20:32:03 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/10 20:32:10 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/10 21:02:44 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
