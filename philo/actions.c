c775378f5dbebb5f29d4222d2e60f57d64312b59
Niklas Weber <nweber@student.42heilbronn.de>
2025-08-15 11:25:59 +0200

    fixed the code to finally work

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 09:52:56 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/12 12:52:40 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/15 11:22:56 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ void	take_forks(t_philo *philo)
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, "has taken a fork");
+		print_status(philo, "has taken the left fork");
 		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, "has taken a fork");
+		print_status(philo, "has taken the right fork");
 	}
 	else
 	{
 		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, "has taken a fork");
+		print_status(philo, "has taken the right fork");
 		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, "has taken a fork");
+		print_status(philo, "has taken the left fork");
 	}
 }
 
@@ -63,6 +63,7 @@ void	*philo_routine(void *arg)
 		take_forks(philo);
 		eat_and_sleep(philo);
 		print_status(philo, "is thinking");
+		usleep(500);
 	}
 	return (NULL);
 }
--- philo/simulation.c
+++ philo/simulation.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 20:32:50 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/12 12:45:30 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/15 11:23:16 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ int	create_threads(t_data *data)
 		if (pthread_create(&data->philos[i].thread, NULL, \
 			philo_routine, &data->philos[i]) != 0)
 			return (0);
-			i++;
+		i++;
 	}
 	if (pthread_create(&data->monitor_thread, NULL, \
 		monitoring, data) != 0)
